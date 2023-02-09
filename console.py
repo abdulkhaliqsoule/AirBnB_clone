810b8cd6ca4045c1953720cc42e0a29b545bbe63
AbdulRaufAl-hassan101010 <abdulraufalhassan101010@gmail.com>
2023-02-09 17:43:25 +0000

    created the console

---
+++ console.py
@@ -0,0 +1,25 @@
+import cmd
+from turtle import *
+
+
+class TurtleShell(cmd.Cmd):
+    intro = ''
+    prompt = '(hbnb) '
+    file = None
+
+    # ----- basic turtle commands -----
+    def do_EOF(self, arg):
+        'Move the turtle forward by the specified distance:  FORWARD 10'
+
+    def do_quit(self, arg):
+        'exit the console'
+        return True
+
+
+def parse(arg):
+    'Convert a series of zero or more numbers to an argument tuple'
+    return tuple(map(int, arg.split()))
+
+
+if __name__ == '__main__':
+    TurtleShell().cmdloop()
